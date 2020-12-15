b70349c17e0bd584119b7860979815b3e45da418
Salmaan <salmaan.arif.ali@gmail.com>
2020-12-15 05:31:08 -0500

    commit tree b2e86f512611cc79b5df498819a26131049a9eec

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if number > 1
+    i = 2
+
+    while i < number
+      return false if number % i == 0
+      i += 1
+    end
+
+    true
+  else
+    false
+  end
+end
